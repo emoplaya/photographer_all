bd1f1e8c397f981e95a7a9979695ea8cfe8e3392
Ivan Kashcheev <basilirs69@gmail.com>
2024-05-13 21:54:48 +0300

    fixes

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const URL = "mongodb://localhost:27017/photographer";
-
+//"mongodb://admin:KNt9%25hsI@oyoliyil.beget.app/admin"
 const connectToDb = async () => {
   await mongoose.connect(URL).then(() => console.log("connected"));
 };
