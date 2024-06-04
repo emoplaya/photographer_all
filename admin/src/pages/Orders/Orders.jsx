ac6ae02dad45018d3f8db654d4cf3f11cc84fbcb
Ivan Kashcheev <basilirs69@gmail.com>
2024-06-04 18:47:54 +0300

    added payment logic

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const Orders = ({ url }) => {
-  return <div>orders</div>;
+  const [orders, setOrders] = useState([]);
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value,
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+  return (
+    <div className="order add">
+      <h3>Заказы</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src="" alt="" />
+            <div>
+              <p className="order-item-photos">
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + "x" + item.quantity;
+                  } else {
+                    return item.name + "x" + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>{order.address.email}</p>
+            <p>Количество: {order.items.length}</p>
+            <p>{order.amount} ₽</p>
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+            >
+              <option value="В обработке">В обработке</option>
+              <option value="Обрабатывается">Обрабатывается</option>
+              <option value="Доставлен">Доставлен</option>
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,8 +1,14 @@
 const orderModel = require("../models/orderModel");
 const userModel = require("../models/userModel");
 const Stripe = require("stripe");
+require("dotenv").config({ path: "./.env" });
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// placing user order for frontend
 const placeOrder = async (req, res) => {
+  const frontend_url = "http://localhost:3000";
+
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
@@ -13,12 +19,87 @@ const placeOrder = async (req, res) => {
     await newOrder.save();
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
-    //payment logic
+    const line_items = req.body.items.map(
+      (item) => ({
+        price_data: {
+          currency: "rub",
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+      })
+      //payment logic
+    );
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    res.json({ success: true, session_url: session.url });
   } catch (error) {
-    console.log({ success: false, message: "Error" });
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success == "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+//Listing orders for admin panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+//api for updating order status
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
   }
 };
 
 module.exports = {
   placeOrder,
+  verifyOrder,
+  userOrders,
+  listOrders,
+  updateStatus,
 };
